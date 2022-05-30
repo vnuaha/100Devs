7c267a5b6e3d38145da90d89ed29314b5c24aafc
viola <nuahavyll@gmail.com>
2022-05-30 10:35:34 +0300

    calculating with functions

--- codewars/Week 21/JavaScript Rank Up Search 5 kyu Calculating with Functions.js
+++ codewars/Week 21/JavaScript Rank Up Search 5 kyu Calculating with Functions.js
@@ -14,3 +14,57 @@
 //     Division should be integer division. For example, this should return 2, not 2.666666...:
 
 // eight(dividedBy(three()));
+
+
+function zero(func) {
+    return func ? func(0) : 0;
+  }
+  function one(func) {
+    return func ? func(1) : 1;
+  }
+  function two(func) {
+    return func ? func(2) : 2;
+  }
+  function three(func) {
+    return func ? func(3) : 3;
+  }
+  function four(func) {
+    return func ? func(4) : 4;
+  }
+  function five(func) {
+    return func ? func(5) : 5;
+  }
+  function six(func) {
+    return func ? func(6) : 6;
+  }
+  function seven(func) {
+    return func ? func(7) : 7;
+  }
+  function eight(func) {
+    return func ? func(8) : 8;
+  }
+  function nine(func) {
+    return func ? func(9) : 9;
+  }
+  
+  function plus(b) {
+    return function(a) {
+      return a + b;
+    };
+  }
+  function minus(b) {
+    return function(a) {
+      return a - b;
+    };
+  }
+  function times(b) {
+    return function(a) {
+      return a * b;
+    };
+  }
+  function dividedBy(b) {
+    return function(a) {
+      return a / b;
+    };
+  }
+  
