ccfd55a234858823f2c5b04d3db800120b5cac10
viola <nuahavyll@gmail.com>
2022-06-10 09:51:03 +0300

    chain adding

---
+++ codewars/5KYU/(Ready for) Prime Time.js
@@ -0,0 +1,42 @@
+/*
+Description:
+We need prime numbers and we need them now!
+
+Write a method that takes a maximum bound and returns all primes up to and including the maximum bound.
+
+For example:
+
+prime(11);
+Should return an array that looks like this:
+
+[2, 3, 5, 7, 11]
+*/
+
+// Define range  <=n
+//Define a prime 
+// 1  || 2 
+
+//Test for prime 
+
+//Return primes - store in an array
+
+
+
+function prime(num) {
+    let primes = [];
+    let i = 1;
+    while (i <= num) {
+      if (isPrime(i)) primes.push(i);
+      i++;
+    }
+    return primes;
+  }
+  
+  function isPrime(n) {
+    let rt = Math.sqrt(n);
+    for (let i = 2; i <= rt; i++) {
+      if (n % i === 0) return false;
+    }
+    return n !== 1;
+  }
+  
